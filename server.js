667eca26f2753654f6c1141dc36bd828b12e341a
Mubarak Ahmad <ahmadmubarak6996@gmail.com>
2021-06-04 00:15:09 +0400

    connected server to mongodb collections

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 // config Express.js
 app.use(express.json())
 app.set('port', 3000)
-app.use ((req,res,next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
@@ -17,8 +17,8 @@ app.use ((req,res,next) => {
 // connect to MongoDB
 const MongoClient = require('mongodb').MongoClient;
 let db;
-MongoClient.connect('mongodb+srv://MyMongoDBUser:wednesday@cluster0.epqbr.mongodb.net', (err, client) => {
-    db = client.db('webstore')
+MongoClient.connect('mongodb+srv://meex:12345@cluster0.orkke.mongodb.net', (err, client) => {
+    db = client.db('coursework2')
 })
 
 
@@ -44,35 +44,35 @@ app.get('/collection/:collectionName', (req, res, next) => {
 
 //adding post
 app.post('/collection/:collectionName', (req, res, next) => {
-req.collection.insert(req.body, (e, results) => {
-if (e) return next(e)
-res.send(results.ops)
-})
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
 })
 
 // return with object id 
 
 const ObjectID = require('mongodb').ObjectID;
 app.get('/collection/:collectionName/:id'
-, (req, res, next) => {
-req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
-if (e) return next(e)
-res.send(result)
-})
-})
+    , (req, res, next) => {
+        req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send(result)
+        })
+    })
 
 
 //update an object 
 
 app.put('/collection/:collectionName/:id', (req, res, next) => {
-req.collection.update(
-{_id: new ObjectID(req.params.id)},
-{$set: req.body},
-{safe: true, multi: false},
-(e, result) => {
-if (e) return next(e)
-res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
-})
+    req.collection.update(
+        { _id: new ObjectID(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
+        })
 })
 
 
@@ -80,12 +80,12 @@ res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
 
 
 app.delete('/collection/:collectionName/:id', (req, res, next) => {
-req.collection.deleteOne(
-{ _id: ObjectID(req.params.id) },(e, result) => {
-if (e) return next(e)
-res.send((result.result.n === 1) ?
-{msg: 'success'} : {msg: 'error'})
-})
+    req.collection.deleteOne(
+        { _id: ObjectID(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+                { msg: 'success' } : { msg: 'error' })
+        })
 })
 
 
